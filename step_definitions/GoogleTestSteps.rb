0270df23ab48a36f161fdc657f11b1f78c1bf565
saurabhsahugit <saurabh.newhorizons@gmail.com>
2014-10-27 15:13:00 +0000

    2

---
+++ step_definitions/GoogleTestSteps.rb
@@ -0,0 +1,27 @@
+require 'selenium-webdriver'
+require 'cucumber'
+require 'capybara'
+require 'capybara/cucumber'
+
+Given(/^I am on Google homepage$/) do
+
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 300)
+profile = Selenium::WebDriver::Firefox::Profile.new
+
+@driver = Selenium::WebDriver.for :firefox, :profile => profile
+
+#@driver = Capybara.default_driver = :selenium
+
+
+#wait.until { @driver.title.downcase.start_with? "my page title" }
+
+
+  puts "MY TEST STEPS"
+
+ #driver.get('http://en.wikipedia.org/wiki/Baltimore_Ravens')
+    #driver.get("http://www.google.com");
+
+   @driver.get("http://www.test.com")
+
+end
